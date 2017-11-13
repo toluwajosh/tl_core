4be58f8415d45ffbb5129c6463b37a09c9030efe
toluwajosh <tjosh.owoyemi@gmail.com>
2017-11-13 16:21:09 +0900

    feat: implemented model testing from meta_graph

--- tl_main.py
+++ tl_main.py
@@ -25,24 +25,14 @@ class TL_model(object):
     self.keep_prob = graph.get_tensor_by_name('keep_prob:0')
     self.output_tensor = graph.get_tensor_by_name('layer'+str(layer_out)+'_out:0')
 
-    # # show some output
-    # print('Model Properties: \n \
-    #   Image Input Size: {} \
-    #   Keep Probability: {} \
-    #   Output Tensor Size: {}'.format(self.image_input, 
-    #                                   self.keep_prob, 
-    #                                   self.output_tensor))
-
   def layers(self, fc_layers, dropouts, mode):
     """
     :param fc_layers: list containing number of units in the fully connected layers
     :param dropouts: list containing dropout probability for each fully connected layer
+    :param mode: the mode of the network
     """
     assert (len(fc_layers) == len(dropouts)), \
           "The Size of fully connected layers and dropouts are not equal"
-    # fc_layer = tf.layers.
-
-    # dense = tf.reshape(self.output_tensor, [-1, 4096])
 
     dense = tf.layers.dense(inputs=self.output_tensor, units=fc_layers[0], 
                             activation=tf.nn.relu)
@@ -58,28 +48,31 @@ class TL_model(object):
     self.logits = tf.layers.dense(inputs=dropout, units=fc_layers[-1])
     self.num_classes = fc_layers[-1]
 
-    return self.logits
 
   def train(self, sess, X_train, y_train, X_validate=None, y_validate=None, 
             epochs=30, batch_size=100, keep_prob=0.4, learning_rate=None,
             save_checkpoint=None):
     """
     Train neural network and print out the loss during training.
-    :param sess: TF Session
-    :param X_train, y_train: training data
-    :param X_train, y_train: validation data
-    :param epochs: Number of epochs
-    :param batch_size: Batch size
-    :param keep_prob: dropout keep probability
-    :param learning_rate: training learning rate
+    
+    Args:
+      sess: TF Session
+      X_train, y_train: training data
+      X_train, y_train: validation data
+      epochs: Number of epochs
+      batch_size: Batch size
+      keep_prob: dropout keep probability
+      learning_rate: training learning rate
+    
+    Returns:
     """
     label = tf.placeholder(tf.int32, shape=(None))
-    self.correct_label = tf.one_hot(label, self.num_classes)
+    correct_label = tf.one_hot(label, self.num_classes)
 
     # loss function:
     cross_entropy_loss = tf.reduce_mean(
         tf.nn.softmax_cross_entropy_with_logits(
-            labels=self.correct_label, logits=self.logits))
+            labels=correct_label, logits=self.logits))
 
     # set up training
     with tf.name_scope("training"):
@@ -107,7 +100,7 @@ class TL_model(object):
 
         _, loss = sess.run([train_op, cross_entropy_loss], 
             feed_dict={self.image_input:new_X_train[offset:end], 
-                        self.correct_label:new_y_train[offset:end], self.keep_prob:keep_prob}) # , learning_rate:1e-4
+                        correct_label:new_y_train[offset:end], self.keep_prob:keep_prob}) # , learning_rate:1e-4
 
         print("epoch: {}, batch: {}, loss: {}".format(epoch+1, current_batch, loss))
       if X_validate.any()!=None:
@@ -115,7 +108,7 @@ class TL_model(object):
         validation_accuracy = self.evaluate(X_validate, y_validate)
         print("Validation Accuracy: ", validation_accuracy)
       if save_checkpoint:
-        saver.save(sess, './data/model_saves/test_train.ckpt')
+        saver.save(sess, save_checkpoint)
 
 
   def evaluate(self, X_data, y_data, keep_prob=1.0):
@@ -124,7 +117,6 @@ class TL_model(object):
 
     correct_label = tf.placeholder(tf.int64, shape=(None))
     
-    # correct_prediction = tf.equal(tf.argmax(self.logits, 1), tf.argmax(self.correct_label, 1))
     correct_prediction = tf.equal(tf.argmax(self.logits, 1), correct_label)
     accuracy_operation = tf.reduce_mean(tf.cast(correct_prediction, tf.float32))
     
@@ -140,6 +132,21 @@ class TL_model(object):
       total_accuracy += (accuracy * batch_size)
     return (total_accuracy / num_examples)
 
+  def test(self, X_data, y_data, model_save_path=None, keep_prob=1.0):
+    if model_save_path:
+      try:
+        meta_graph_file = model_save_path+'.meta'
+        with tf.Session() as sess:
+          sess.run(tf.global_variables_initializer())
+          print("meta_graph_file: ",meta_graph_file)
+          new_saver = tf.train.import_meta_graph(meta_graph_file)
+          new_saver.restore(sess, model_save_path)
+
+          accuracy = self.evaluate(X_data, y_data)
+          print("Test Accuracy: ", accuracy)
+      except Exception as e:
+        print("\nNo previous model found, or saved model specified!")
+        raise
 
 if __name__ == '__main__':
   data_dir = './data'
@@ -173,6 +180,7 @@ if __name__ == '__main__':
 
   y_train = to_categorical(y_train, num_classes)
   y_validate = to_categorical(y_validate, num_classes)
+  y_test = to_categorical(y_test, num_classes)
 
   print("\nsize of training labels: {}".format(np.shape(y_train)))
 
@@ -189,7 +197,7 @@ if __name__ == '__main__':
   model_tag = 'vgg16'
   layer_out = 7
   image_shape = (224, 224)
-  epochs = 30
+  epochs = 1
   batch_size = 250
 
   with tf.Session() as sess:
@@ -207,51 +215,10 @@ if __name__ == '__main__':
     mode = tf.estimator.ModeKeys.TRAIN
     # logits = tl_model.layers(fc_layers, dropouts, mode)
     tl_model.layers(fc_layers, dropouts, mode)
-    tl_model.train(sess, 
-                    X_train, y_train, 
-                    X_validate, y_validate, 
-                    batch_size=batch_size)
-
-    # print("\nLogits: ", logits)
-
-    # # logits = tf.reshape(logits, (-1, num_classes))
-    # # correct_label = tf.reshape(label, (-1, num_classes))
-    # label = tf.placeholder(tf.int32, shape=(None))
-    # correct_label = tf.one_hot(label, num_classes)
-
-    # input_image = tl_model.image_input
-    # keep_prob = tl_model.keep_prob
-
-    # print("\nInput image shape: ", input_image)
-
-
-    # cross_entropy_loss = tf.reduce_mean(
-    #     tf.nn.softmax_cross_entropy_with_logits(
-    #         labels=correct_label, logits=logits))
-
-    # with tf.name_scope("training"):
-    #   optimizer = tf.train.AdamOptimizer()
-    #   # Create a variable to track the global step.
-    #   global_step = tf.Variable(0, name='global_step', trainable=False)
-    #   # Use the optimizer to apply the gradients that minimize the loss
-    #   # (and also increment the global step counter) as a single training step.
-    #   train_op = optimizer.minimize(cross_entropy_loss, global_step=global_step)
-
-
-    # # to save the trained model (preparation)
-    # saver = tf.train.Saver()
-
-    # sess.run(tf.global_variables_initializer())
-    # # print("\n Training... \n")
-    # for epoch in range(epochs):
-    #   new_X_train, new_y_train = shuffle(X_train, y_train)
-    #   # Training, use batches
-    #   for offset in range(0, num_examples, batch_size):
-    #     end = offset + batch_size
-
-    #     _, loss = sess.run([train_op, cross_entropy_loss], 
-    #         feed_dict={input_image:new_X_train[offset:end], 
-    #                     correct_label:new_y_train[offset:end], keep_prob:0.4}) # , learning_rate:1e-4
-
-    #     print("epoch: {}, batch: {}, loss: {}".format(epoch+1, epoch, loss))
-    #   saver.save(sess, './data/model_saves/test_train.ckpt')
+    # tl_model.train(sess, 
+    #                 X_train, y_train, 
+    #                 X_validate, y_validate,
+    #                 epochs=epochs,
+    #                 batch_size=batch_size,
+    #                 save_checkpoint='./data/model_saves/test_train.ckpt')
+    tl_model.test(X_test, y_test, model_save_path='./data/model_saves/test_train.ckpt')
