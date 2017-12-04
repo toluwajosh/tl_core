767347af955fa9d20df02fbd799ad3edead8765b
toluwajosh <tjosh.owoyemi@gmail.com>
2017-12-04 21:19:56 +0900

    checkpoint: 041117

--- utils/lin_reg.py
+++ utils/lin_reg.py
@@ -4,6 +4,8 @@ import matplotlib.pyplot as plt
 plt.rcParams['figure.figsize'] = (20.0, 10.0)
 from mpl_toolkits.mplot3d import Axes3D
 
+# from the tutorial on: https://mubaris.com/2017-09-28/linear-regression-from-scratch
+# the code here is incomplete
 
 X = np.array([x0, math, read]).T
 B = np.array([0, 0, 0])
@@ -36,3 +38,27 @@ def gradient_descent(X, Y, B, alpha, iterations):
     cost_history[iteration] = cost
     
   return B, cost_history
+
+
+# 100000 Iterations
+newB, cost_history = gradient_descent(X, Y, B, alpha, 100000)
+
+
+# New Values of B
+print(newB)
+
+# Final Cost of new B
+print(cost_history[-1])
+
+# Model Evaluation - R2 Score
+def r2_score(Y, Y_pred):
+    mean_y = np.mean(Y)
+    ss_tot = sum((Y - mean_y) ** 2)
+    ss_res = sum((Y - Y_pred) ** 2)
+    r2 = 1 - (ss_res / ss_tot)
+    return r2
+
+Y_pred = X.dot(newB)
+
+print(rmse(Y, Y_pred))
+print(r2_score(Y, Y_pred))
